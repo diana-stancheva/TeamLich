179b6d0a17b92e3186a8f965c5225405e1789b66
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-08-29 19:58:01 +0300

    initial

---
+++ AndOneConstructions/AndOneConstructions.ConsoleClient/EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿namespace AndOneConstructions.ConsoleClient
+{
+    using AndOneConstructions.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class EntryPoint
+    {
+        static void Main()
+        {
+            var db = new AndOneConstructionsDataContext();
+
+            var buildings = db.Buildings.ToArray();
+
+            // for testing
+            foreach (var b in buildings)
+            {
+                Console.WriteLine(b.Name);
+            }
+        }
+    }
+}
