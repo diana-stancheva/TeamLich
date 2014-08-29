3e84e951aa35b57ced6f6058f6b4afcfe50f3061
Diana Stancheva <diana@calderra.com>
2014-08-30 01:16:15 +0300

    READ COMMENT IN EntryPoint.cs

--- AndOneConstructions/AndOneConstructions.ConsoleClient/EntryPoint.cs
+++ AndOneConstructions/AndOneConstructions.ConsoleClient/EntryPoint.cs
@@ -1,4 +1,29 @@
-﻿namespace AndOneConstructions.ConsoleClient
+﻿/*
+ * 
+ * АКО НЯКОЙ НЕ Е ПРОЧЕЛ В SKYPE:
+ * 
+ * Използваме хостинга на App Harbor за всички database сървъри,
+ * за да нямаме проблеми с coonnection стринговете и с различните версии на SQL SERVER
+ * и за да не се налага да прехвърляме постоянно бази данни през github.
+ * 
+ * Тествала съм, според мен работи добре, вижте дали всичко е наред при вас
+ * 
+ * Контекста за работа с SQL базата в App Harbor e:
+ * AndOneConstructions.Model.AndOneConstructionsContext()
+ * 
+ * AndOneConstructions.Data.AndOneConstructionsDataContext() - > Работи с локалната база данни
+ * /и ако не сте с SQLEXPRESS 2014 трябва да промените connection стринга в app.config/
+ * Оставила съм го за сега, когато видим, че всичко е ок с App Harbor ще го махнем.
+ * 
+ * За връзка с SQL през Management Studio:
+ * 
+ * Server:      d80b5849-dd81-49dc-acdc-a396015983af.sqlserver.sequelizer.com
+ * Login:     	rqhqhjzlydkhrstg 	
+ * Password: 	6vULZxqG4R6jrzudTKdpEpKo6o5EnDiztsPaDvBqvTTGzUkFVuFfNERQSEPLATux
+ * 
+ * 
+ */
+namespace AndOneConstructions.ConsoleClient
 {
     using AndOneConstructions.Model;
     using System;
@@ -16,32 +41,32 @@
     {
         static void Main()
         {
-            var db = new AndOneConstructionsContext();
+            //var db = new AndOneConstructionsContext();
 
-            var buildings = db.Buildings.ToArray();
+            //var buildings = db.Buildings.ToArray();
 
-            // for testing
-            foreach (var b in buildings)
-            {
-                Console.WriteLine(b.Name);
-            }
+            //// for testing
+            //foreach (var b in buildings)
+            //{
+            //    Console.WriteLine(b.Name);
+            //}
 
             ////TEST MONGO
-            var test = new MongoDBEmployee();
-            test.PrintAllEntities();
+            //var test = new MongoDBEmployee();
+            //test.PrintAllEntities();
 
             //Test Mongo Import
 
-            //ImportDataController.ImportMongoDBEmployees();
+            ImportDataController.ImportMongoDBEmployees();
 
-            //using (var db = new AndOneConstructionsContext())
-            //{
-            //    var employees = db.Employees.ToList();
-            //    foreach (var item in employees)
-            //    {
-            //        Console.WriteLine(item.FirstName + ' ' + item.LastName);
-            //    }
-            //}
+            using (var db = new AndOneConstructionsContext())
+            {
+                var employees = db.Employees.ToList();
+                foreach (var item in employees)
+                {
+                    Console.WriteLine(item.FirstName + ' ' + item.LastName);
+                }
+            }
 
         }
     }
