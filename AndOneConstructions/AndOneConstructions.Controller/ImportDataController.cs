10b0a1580b431ee8026fb16276cbccff6d95c0a3
Diana Stancheva <diana@calderra.com>
2014-08-29 21:39:10 +0300

    added connection to mongodb

---
+++ AndOneConstructions/AndOneConstructions.Controller/ImportDataController.cs
@@ -0,0 +1,21 @@
+﻿namespace AndOneConstructions.Controller
+{
+    
+    using System;
+    using System.Linq;
+
+    using AndOneConstructions.Data;
+
+    public static class ImportDataController
+    {
+        public static void ImportDataFromMongoDB()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ImportDataFromZIP()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AndOneConstructions/MongoDB.Data/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿namespace MongoDB.Data
+{
+    using MongoDB.Driver;
+    using System;
+    using System.Linq;
+
+    public static class DBConnection
+    {
+        public static MongoDatabase GetDBConnection(string databaseName)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase(databaseName);
+
+            return database;
+        }
+    }
+}
