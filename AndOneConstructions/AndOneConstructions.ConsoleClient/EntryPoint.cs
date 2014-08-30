7cf889ad8c3ee415da0a18c209e22d2759b39699
Diana Stancheva <diana@calderra.com>
2014-08-30 10:01:19 +0300

    update mongo import

--- AndOneConstructions/AndOneConstructions.ConsoleClient/EntryPoint.cs
+++ AndOneConstructions/AndOneConstructions.ConsoleClient/EntryPoint.cs
@@ -37,47 +37,42 @@
 namespace AndOneConstructions.ConsoleClient
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using MongoDB.Data.Context;
 
     using AndOneConstructions.Controller;
-    using AndOneConstructions.Data;
     using AndOneConstructions.Model;
 
     class EntryPoint
     {
         static void Main()
         {
-            //TEST SQL 
-            var db = new AndOneConstructionsContext();
+            ////TEST SQL 
+            //var db = new AndOneConstructionsContext();
 
-            var buildings = db.Buildings.ToArray();
+            //var buildings = db.Buildings.ToArray();
 
-            // for testing
-            foreach (var b in buildings)
-            {
-                Console.WriteLine(b.Name);
-            }
+            //// for testing
+            //foreach (var b in buildings)
+            //{
+            //    Console.WriteLine(b.Name);
+            //}
 
             //TEST MONGO
-            var test = new MongoDBEmployee();
-            test.PrintAllEntities();
+            //var test = new MongoDBEmployee();
+            //test.PrintAllEntities();
 
-            ////Test Mongo Import
+            //Test Mongo Import
 
-            //ImportDataController.ImportMongoDBEmployees();
+            ImportDataController.ImportMongoDBEmployees();
 
-            //using (var db = new AndOneConstructionsContext())
-            //{
-            //    var employees = db.Employees.ToList();
-            //    foreach (var item in employees)
-            //    {
-            //        Console.WriteLine(item.FirstName + ' ' + item.LastName);
-            //    }
-            //}
+            using (var db = new AndOneConstructionsContext())
+            {
+                var employees = db.Employees.ToList();
+                foreach (var item in employees)
+                {
+                    Console.WriteLine(item.FirstName + ' ' + item.LastName);
+                }
+            }
 
         }
     }
--- AndOneConstructions/AndOneConstructions.Controller/ImportDataController.cs
+++ AndOneConstructions/AndOneConstructions.Controller/ImportDataController.cs
@@ -1,12 +1,12 @@
 ﻿namespace AndOneConstructions.Controller
 {
-    
     using System;
     using System.Linq;
 
     using MongoDB.Data.Context;
-    using AndOneConstructions.Model;
     using MongoDB.Driver;
+
+    using AndOneConstructions.Model;
 
     public static class ImportDataController
     {
@@ -19,16 +19,24 @@
 
                 foreach (var emp in employeeCollection.FindAll())
                 {
-                    var isInDB = db.Employees.Select(e => e.FirstName == emp.FirstName && e.LastName == emp.LastName).FirstOrDefault();
+                    var isInDB = db.Employees.Where(e => e.FirstName == emp.FirstName && e.LastName == emp.LastName);
 
-                    if (isInDB == null)
+                    //TODO: Check for Department!!!
+
+                    if (isInDB.Count() == 0)
                     {
                         db.Employees.Add(new Employee
                         {
                             FirstName = emp.FirstName,
                             LastName = emp.LastName,
-                            Department = new Department { Name = emp.Department}
+                            Department = new Department { Name = emp.Department }
                         });
+
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Employee {0} Exists in DB", emp.FirstName + " " + emp.LastName);
                     }
                 }
             }
