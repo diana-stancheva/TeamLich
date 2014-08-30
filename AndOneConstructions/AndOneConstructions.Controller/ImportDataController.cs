11a6c19819eb65d87b378fc2e35036991feeb4f6
Diana Stancheva <diana@calderra.com>
2014-08-30 11:34:55 +0300

    check for existing department
    employees import from mongo

--- AndOneConstructions/AndOneConstructions.Controller/ImportDataController.cs
+++ AndOneConstructions/AndOneConstructions.Controller/ImportDataController.cs
@@ -12,18 +12,21 @@
     {
         public static void ImportMongoDBEmployees()
         {
-            using (var db = new AndOneConstructionsContext() )
+            using (var db = new AndOneConstructionsContext())
             {
                 var employee = new MongoDBEmployee();
                 MongoCollection<MongoDBEmployee> employeeCollection = employee.GetAllEntitiesAsCollection();
 
                 foreach (var emp in employeeCollection.FindAll())
                 {
-                    var isInDB = db.Employees.Where(e => e.FirstName == emp.FirstName && e.LastName == emp.LastName);
+                    var isEmployeeInDB = db.Employees.Where(e => e.FirstName == emp.FirstName && e.LastName == emp.LastName);
+                    
+                    var isDepartmentInDB = db.Departments.Where(d => d.Name == emp.Department);
 
-                    //TODO: Check for Department!!!
+                    Console.WriteLine("empl {0}", isEmployeeInDB.Count());
+                    Console.WriteLine("dep {0}", isDepartmentInDB.Count());
 
-                    if (isInDB.Count() == 0)
+                    if (isEmployeeInDB.Count() == 0 && isDepartmentInDB.Count() == 0)
                     {
                         db.Employees.Add(new Employee
                         {
@@ -34,11 +37,33 @@
 
                         db.SaveChanges();
                     }
+                    else if (isEmployeeInDB.Count() == 0 && isDepartmentInDB.Count() > 0)
+                    {
+                        var departmentid = isDepartmentInDB.FirstOrDefault().DepartmentId;
+
+                        db.Employees.Add(new Employee
+                        {
+                            FirstName = emp.FirstName,
+                            LastName = emp.LastName,
+                            DepartmentId = departmentid
+                        });
+                        db.SaveChanges();
+
+                        Console.WriteLine("{0} added -> department id {1}", emp.FirstName + " " + emp.LastName, departmentid);
+                    }
                     else
                     {
                         Console.WriteLine("Employee {0} Exists in DB", emp.FirstName + " " + emp.LastName);
                     }
                 }
+            }
+        }
+
+        public static void ImportMongoDBProjects()
+        {
+            using (var db = new AndOneConstructionsContext())
+            {
+                
             }
         }
 
