c2abe6810d93ffeb5694333b25e17ff8bf5ce06a
Martin Anastasov <m4rt00@gmail.com>
2014-09-02 15:06:11 +0300

    Added SecretDB.Data

---
+++ AndOneConstructions/SecretDB.Data/SecretDBContext.cs
@@ -0,0 +1,15 @@
+﻿namespace SecretDB.Data
+{
+    using SecretDB.Models;
+    using System.Data.Entity;
+
+    public class SecretDBContext : DbContext
+    {
+        public SecretDBContext()
+            : base("SecretDBConnectionString")
+        {
+        }
+
+        public IDbSet<Bribe> Bribes { get; set; }
+    }
+}
