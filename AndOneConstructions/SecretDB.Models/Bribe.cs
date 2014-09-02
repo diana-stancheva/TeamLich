a39748be2e109e1234c89655d3a7a4e7c8f3e1c0
Martin Anastasov <m4rt00@gmail.com>
2014-09-02 15:00:34 +0300

    Added SecretDB.Models

---
+++ AndOneConstructions/SecretDB.Models/Bribe.cs
@@ -0,0 +1,24 @@
+﻿namespace SecretDB.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    
+    public class Bribe
+    {
+        [Key]
+        public int BribeId { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public string ProjectName { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? LastUpdate { get; set; }
+    }
+}
